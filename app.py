bf1b2963d124e9aa30a7e74322e39d6b9089653b
Rebecca Tracey-Timoney <71783662+rebeccatraceyt@users.noreply.github.com>
2021-05-22 19:33:46 +0100

    Initial Commit - app.py setup

---
+++ app.py
@@ -0,0 +1,20 @@
+from logging import debug
+import os
+from flask import Flask
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World, again"
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True) # MAKE SURE IT IS FALSE
+
