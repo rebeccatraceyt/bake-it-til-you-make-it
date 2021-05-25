b4a874fa5d223c498d0263e37f5a2dc4399ddf0d
Rebecca Tracey-Timoney <71783662+rebeccatraceyt@users.noreply.github.com>
2021-05-25 22:54:20 +0100

    Fixed problem with navbar collapse - human error

--- static/js/script.js
+++
@@ -1,9 +0,0 @@
-
-$(function () {
-    /* Changes background of navigation bar on scroll for UX purposes.
-      Sourced from https://jsfiddle.net/we9L9h2r/ */
-    $(document).scroll(function () {
-        var $nav = $(".scroll-nav");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
-});
