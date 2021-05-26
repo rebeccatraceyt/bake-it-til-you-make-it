7711ab55d1be21a8ae06bcf67dc701edce5adafd
Rebecca Tracey-Timoney <71783662+rebeccatraceyt@users.noreply.github.com>
2021-05-26 18:06:42 +0100

    Resolved design issues by restyling the template

--- static/js/script.js
+++ static/js/script.js
@@ -5,3 +5,12 @@ function showDisclaimer() {
 }
 
 document.getElementById("date-text").textContent = new Date().getFullYear();
+
+$(function () {
+    /*Changes background of navigation bar on scroll for UX purposes.
+        Sourced from https://jsfiddle.net/we9L9h2r/ */
+    $(document).scroll(function () {
+        var $nav = $(".scroll-nav");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
