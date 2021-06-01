16dd9ee676eb2f45152febca52c0a02aa1b3b2fb
Rebecca Tracey-Timoney <71783662+rebeccatraceyt@users.noreply.github.com>
2021-06-01 15:47:44 +0100

    Added login.html functionality

--- app.py
+++ app.py
@@ -75,6 +75,38 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """
+    Allows users to login to account
+    """
+    if request.method == "POST":
+        # checking if user exists
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username".lower())})
+        
+        if existing_user:
+            # stored hash password must match user input
+            if check_password_hash(
+                existing_user["password"], request.form.get(
+                    "password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome back, {}".format(
+                    request.form.get("username")))
+                return redirect(url_for(
+                    "profile", username=session["user"]))
+            else:
+                # if the password does not match
+                flash("Incorrect Username and/or Password, please try again")
+                return redirect(url_for("login"))
+        else:
+            # username does not exist
+            flash("Incorrect Username and/or Password, please try again")
+            return redirect(url_for("login"))
+    
+    return render_template("login.html")
+                
+
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     """
